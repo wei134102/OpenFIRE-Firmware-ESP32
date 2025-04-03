eaec3096703d5da79ebb24abb565ac11cb35a8fe
alessandro-satanassi <alessandro@cittini.it>
2025-04-03 12:51:29 +0200

    clean

--- lib/TinyUSB_Devices/TinyUSB_Devices.cpp
+++ lib/TinyUSB_Devices/TinyUSB_Devices.cpp
@@ -12,7 +12,7 @@
 #endif
 
 #include "TinyUSB_Devices.h"
-#if defined(ARDUINO_RASPBERRY_PI_PICO_W) && defined(ENABLE_CLASSIC)
+#if defined(ARDUINO_RASPBERRY_PI_PICO_W) && defined(ENABLE_CLASSIC)  
   #include <HID_Bluetooth.h>
   #include <PicoBluetoothHID.h>
 #endif // ARDUINO_RASPBERRY_PI_PICO_W
