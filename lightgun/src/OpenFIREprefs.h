69c52b228d327a2037ea8ca03bcaeeed2a7ad9ce
alessandro-satanassi <alessandro@cittini.it>
2026-05-17 01:31:31 +0200

    agg

--- lightgun/src/OpenFIREprefs.h
+++ lightgun/src/OpenFIREprefs.h
@@ -87,10 +87,10 @@ public:
 
     /// @brief Instance of profile data 
     static inline ProfileData_t profiles[PROFILE_COUNT] = {
-        {0, 0, 0, 0, 500 << 2, 1420 << 2, 512 << 2, 384 << 2, DFRobotIRPositionEx::Sensitivity_Default, 1, OF_Const::layoutSquare, OF_Const::ar16_9, 0xFF0000, "Profile A"},
-        {0, 0, 0, 0, 500 << 2, 1420 << 2, 512 << 2, 384 << 2, DFRobotIRPositionEx::Sensitivity_Default, 1, OF_Const::layoutSquare, OF_Const::ar16_9, 0x00FF00, "Profile B"},
-        {0, 0, 0, 0, 500 << 2, 1420 << 2, 512 << 2, 384 << 2, DFRobotIRPositionEx::Sensitivity_Default, 1, OF_Const::layoutSquare, OF_Const::ar16_9, 0x0000FF, "Profile Start"},
-        {0, 0, 0, 0, 500 << 2, 1420 << 2, 512 << 2, 384 << 2, DFRobotIRPositionEx::Sensitivity_Default, 1, OF_Const::layoutSquare, OF_Const::ar16_9, 0xFF00FF, "Profile Select"}
+        {0, 0, 0, 0, 500 << 2, 1420 << 2, 512 << 2, 384 << 2, DFRobotIRPositionEx::Sensitivity_Default, FW_Const::RunMode_Normal/*1*/, OF_Const::layoutSquare, OF_Const::ar16_9, 0xFF0000, "Profile A"},
+        {0, 0, 0, 0, 500 << 2, 1420 << 2, 512 << 2, 384 << 2, DFRobotIRPositionEx::Sensitivity_Default, FW_Const::RunMode_Normal/*1*/, OF_Const::layoutSquare, OF_Const::ar16_9, 0x00FF00, "Profile B"},
+        {0, 0, 0, 0, 500 << 2, 1420 << 2, 512 << 2, 384 << 2, DFRobotIRPositionEx::Sensitivity_Default, FW_Const::RunMode_Normal/*1*/, OF_Const::layoutSquare, OF_Const::ar16_9, 0x0000FF, "Profile Start"},
+        {0, 0, 0, 0, 500 << 2, 1420 << 2, 512 << 2, 384 << 2, DFRobotIRPositionEx::Sensitivity_Default, FW_Const::RunMode_Normal/*1*/, OF_Const::layoutSquare, OF_Const::ar16_9, 0xFF00FF, "Profile Select"}
     };
 
     static inline uint currentProfile = 0;
